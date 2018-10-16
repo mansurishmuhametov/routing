f40c3891b2d69395fe3a96d7c6d82b426ed0e237
Ишмухаметов Мансур Рифович <m.ishmuhametov@watcom.ru>
2018-10-16 14:57:29 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
+const appRoutes: Routes = [
+    { path: 'crisis-center', component: CrisisListComponent },
+    { path: 'heroes', component: HeroListComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CrisisListComponent,
+        HeroListComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true }
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
