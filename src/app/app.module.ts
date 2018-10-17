0c08a3315a7d01fb29b87f924acf670614cc45cd
Ишмухаметов Мансур Рифович <m.ishmuhametov@watcom.ru>
2018-10-17 15:43:21 +0300

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
-
-const appRoutes: Routes = [
-    { path: 'crisis-center', component: CrisisListComponent },
-    { path: 'heroes', component: HeroListComponent },
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
     declarations: [
         AppComponent,
         CrisisListComponent,
-        HeroListComponent
+        HeroListComponent,
+        PageNotFoundComponent
     ],
-    imports: [
-        BrowserModule,
-        RouterModule.forRoot(
-            appRoutes,
-            { enableTracing: true }
-        )
-    ],
-    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
